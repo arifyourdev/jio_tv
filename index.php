e78ff361f72410f2e17d79b1e88ec41220786b22
arifyourdev <hariskhan855855@gmail.com>
2024-02-05 01:13:02 +0530

    add

--- index.php
+++ index.php
@@ -9,14 +9,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home </title>
     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
-
-    <h1>My First Heading</h1>
-    <p>My first paragraph.</p>
-
+    <div class="main">
+        <header class="header">
+            <div class="hearder__wrapper">
+                <div class="left__head_wrapper">
+                    <div class="main_logo">
+                        <img src="assets/images/jc_logo_v2.svg" alt="">
+                        <button class="subscription_btn btn">Subscribe</button>
+                    </div>
+                    <div class="header_nav">
+                        <nav>
+                            <ul>
+                                <li><a href="" class="active">Home</a></li>
+                                <li><a href="">Movies</a></li>
+                                <li><a href="">Shows</a></li>
+                                <li><a href="">Sports</a></li>
+                                <li>
+                                    <a>More</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="right_head_wrapper">
+                    <div class="search_wrapper">
+                        <div>
+                            <label for=""></label>
+                            <input type="text" placeholder="Movies, Shows and More">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div class="movie__user_ac">
+                        <span>Login</span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
 </body>
 
 </html>
